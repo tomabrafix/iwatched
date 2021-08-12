6b38a0adc0a6b3fd288bfc0acd566e0ff42f26d1
Tom Kunze <mail@tomabrafix.de>
2021-08-12 14:17:27 +0200

    install.sh: Create missing directory.

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+set -e
+set -u
+
+
 cd "$(dirname "$(realpath "$0")")"
+
 
 docker-compose up -d
 docker-compose run --rm --no-deps iwatched-server composer install
 docker-compose run --rm --no-deps iwatched-server php artisan storage:link
-docker-compose run --rm --no-deps iwatched-server php artisan migrate
+docker-compose run --rm --no-deps iwatched-server php artisan migrate -n --force
+mkdir -p ./storage/app/imdb
 docker-compose run --rm --no-deps iwatched-server php artisan import:titles
 docker-compose run --rm --no-deps iwatched-server php artisan import:elastic
